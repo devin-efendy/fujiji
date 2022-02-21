4360b8f3fac8236d64617d25ddbf2a1c5061bcf0
Jishan Arora <jishan.sikasu@gmail.com>
2022-02-21 01:43:32 -0600

    Navigation bar tweaks

--- fujiji-client/src/components/NavBar/NavBar.jsx
+++ fujiji-client/src/components/NavBar/NavBar.jsx
@@ -1,115 +1,65 @@
 import PropTypes from 'prop-types';
-import {
-  Box,
-  Flex,
-  Avatar,
-  Link,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  Stack,
-} from '@chakra-ui/react';
-import { AddIcon } from '@chakra-ui/icons';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { useState } from 'react';
+import styles from './NavBar.module.css';
 
 const SignInGroup = (
-  <Stack
-    flex={{ base: 1, md: 0 }}
-    justify="flex-end"
-    direction="row"
-    spacing={6}
-  >
-    <Button
-      display={{ base: 'none', md: 'inline-flex' }}
-      as="a"
-      fontSize="sm"
-      fontWeight={400}
-      variant="link"
-      href="#"
-    >
-      Sign Up
-    </Button>
-    <Button
-      fontSize="sm"
-      fontWeight={600}
-      colorScheme="teal"
-      color="white"
-      href="#"
-    >
-      Sign In
-    </Button>
-  </Stack>
+  <ul className={styles.navList}>
+    <li className={styles.navItem}><a href="/#" className={styles.navLink}>Home</a></li>
+    <li className={styles.navItem}><a href="/#" className={styles.navLink}>Search</a></li>
+    <li className={styles.navItem}><a href="/#" className={styles.navLink}>Sign In</a></li>
+  </ul>
 );
 
 const UserGroup = (
-  <Menu>
-    <Button
-      display={{ base: 'none', md: 'inline-flex' }}
-      variant="solid"
-      colorScheme="teal"
-      size="sm"
-      mr={4}
-      leftIcon={<AddIcon />}
-      aria-label="post-listing-button"
-    >
-      Post
-    </Button>
-    <MenuButton
-      as={Button}
-      rounded="full"
-      variant="link"
-      cursor="pointer"
-      minW={0}
-      aria-label="profile-button"
-    >
-      <Avatar aria-label="profile-image" size="sm" alt="Profile image" />
-    </MenuButton>
-    <MenuList>
-      <MenuItem>Home</MenuItem>
-      <MenuItem
-        onClick={() => {
-          // TODO: route to My Listings page
-        }}
-      >
-        My Listings
-      </MenuItem>
-      <MenuDivider />
-      <MenuItem
-        onClick={() => {
-          // TODO: Implement Sign Out
-        }}
-      >
-        Sign Out
-      </MenuItem>
-    </MenuList>
-  </Menu>
+  <ul className={styles.navList}>
+    <li className={styles.navItem}><a href="/#" className={styles.navLink}>Home</a></li>
+    <li className={styles.navItem}><a href="/#" className={styles.navLink}>Search</a></li>
+    <li className={styles.navItem}><a href="/#" className={styles.navLink}>Post</a></li>
+    <li className={styles.navItem}><a href="/#" className={styles.navLink}>Favourites</a></li>
+    <li className={styles.navItem}><a href="/#" className={styles.navLink}>Sign Out</a></li>
+  </ul>
 );
 
 export default function NavBar({ isSignedIn = false }) {
   const rightNavGroup = isSignedIn ? UserGroup : SignInGroup;
+  const [toggleMenu, settoggleMenu] = useState(false);
+  const [showCloseIcon, setShowCloseIcon] = useState(false);
+  const menuClasses = toggleMenu
+    ? `${styles.showMenu} ${styles.navMenu}`
+    : styles.navMenu;
+
+  const closeIconClasses = showCloseIcon
+    ? `${styles.showCloseIcon} ${styles.navClose}`
+    : styles.navClose;
+
+  const openMenu = () => {
+    settoggleMenu(true);
+    setShowCloseIcon(true);
+  };
+
+  const closeMenu = () => {
+    settoggleMenu(false);
+    setShowCloseIcon(false);
+  };
 
   return (
-    <Box bg="gray.100" px={4}>
-      <Flex h={16} alignItems="center" justifyContent="space-between">
-        <Box fontWeight={600} fontSize="24px">
-          <Link
-            aria-label="Marketplace link"
-            px={2}
-            py={1}
-            href="/test/test"
-            _hover={{ textDecoration: 'none' }}
-          >
-            Fujiji
-          </Link>
-        </Box>
-
-        <Flex alignItems="center">
+    <div className={styles.header}>
+      <nav className={`${styles.grid} ${styles.nav}`}>
+        <div>
+          <a href="/#" className={styles.navLogo}>Fujiji</a>
+        </div>
+        <div className={styles.navToggle} id="navToggle" role="presentation" onClick={openMenu}>
+          <HamburgerIcon />
+        </div>
+        <div className={menuClasses} id="navMenu">
+          <div className={closeIconClasses} id="navClose" role="presentation" onClick={closeMenu}>
+            <CloseIcon />
+          </div>
           {rightNavGroup}
-        </Flex>
-      </Flex>
-    </Box>
+        </div>
+      </nav>
+    </div>
   );
 }
 
