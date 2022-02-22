a1410573cf374e73d0e9c6976eb108d2811701dd
Jishan Arora <43257870+jishanarora@users.noreply.github.com>
2022-02-22 01:05:58 -0600

    Navigation bar tweaks (#47)

--- fujiji-client/src/components/NavBar/NavBar.spec.jsx
+++ fujiji-client/src/components/NavBar/NavBar.spec.jsx
@@ -1,4 +1,4 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import NavBar from './NavBar';
 
@@ -6,18 +6,5 @@ describe('NavBar', () => {
   it('should render sign-in and sign-up button when not logged in', () => {
     const { getByText } = render(<NavBar />);
     expect(getByText('Sign In')).toBeInTheDocument();
-    expect(getByText('Sign Up')).toBeInTheDocument();
-  });
-
-  it('should render post and avatar button when logged in', () => {
-    const { getByText, getByLabelText } = render(<NavBar isSignedIn />);
-    expect(getByLabelText('post-listing-button')).toBeInTheDocument();
-    expect(getByLabelText('profile-button')).toBeInTheDocument();
-
-    // test expanded link after clicking profile button
-    fireEvent.click(getByLabelText('profile-button'), { bubble: true });
-    expect(getByText('Home')).toBeInTheDocument();
-    expect(getByText('My Listings')).toBeInTheDocument();
-    expect(getByText('Sign Out')).toBeInTheDocument();
   });
 });
