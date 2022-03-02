f3e09ad5e05642a2a7463f6766576789ce5df5f8
wynonagcc <wynonagoldiella@gmail.com>
2022-03-01 18:59:45 -0600

    Clean up tests for get listings

---
+++ fujiji-server/__test__/controllers/listing.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const app = require('../../src/app');
+
+describe('Test /listing endpoints', () => {
+  describe('GET /listing', () => {
+    it('successfully get all listings by existing city', async () => {
+      const res = await request(app)
+        .get('/listing')
+        .query({ city: 'Winnipeg' });
+      expect(res.statusCode).toEqual(200);
+    });
+
+    it('successfully get all listings by existing province', async () => {
+      const res = await request(app)
+        .get('/listing')
+        .query({ provinceCode: 'MB' });
+      expect(res.statusCode).toEqual(200);
+    });
+
+    it('undefined location returns expected error', async () => {
+      const res = await request(app)
+        .get('/listing');
+      expect(res.statusCode).toEqual(400);
+    });
+  });
+});
