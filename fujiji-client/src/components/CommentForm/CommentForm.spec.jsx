8552f4c0e0136afa4d2d7d80026774d56cb759f2
wynonagcc <wynonagoldiella@gmail.com>
2022-04-02 16:01:13 -0500

    Implemented comment form

---
+++ fujiji-client/src/components/CommentForm/CommentForm.spec.jsx
@@ -0,0 +1,76 @@
+import { render, fireEvent, act } from '@testing-library/react';
+
+import CommentForm from './CommentForm';
+
+const mockDefaultProps = {
+  listingID: 12,
+  userName: 'Jane',
+  onSubmit: jest.fn(),
+};
+
+describe('CommentForm', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render properly', () => {
+    const { getByText } = render(<CommentForm {...mockDefaultProps} />);
+    expect(getByText(mockDefaultProps.userName)).toBeInTheDocument();
+  });
+
+  it('should render input changes in the textarea', () => {
+    const { getByLabelText, getByText } = render(
+      <CommentForm {...mockDefaultProps} />,
+    );
+
+    fireEvent.change(getByLabelText('comment-input'), {
+      target: { value: 'new comment' },
+    });
+
+    expect(getByText('new comment')).toBeInTheDocument();
+  });
+
+  it('should render error message when submitting empty comment', () => {
+    const { getByLabelText, getByText } = render(
+      <CommentForm {...mockDefaultProps} />,
+    );
+
+    fireEvent.click(getByLabelText('submit-comment-button'));
+    expect(getByText("comment can't be empty")).toBeInTheDocument();
+  });
+
+  it('should submit the POST form when comment is valid', async () => {
+    mockDefaultProps.onSubmit.mockResolvedValueOnce({ status: 200 });
+
+    const { getByLabelText } = render(<CommentForm {...mockDefaultProps} />);
+
+    fireEvent.change(getByLabelText('comment-input'), {
+      target: { value: 'new comment' },
+    });
+
+    await act(async () => {
+      fireEvent.click(getByLabelText('submit-comment-button'));
+    });
+
+    expect(getByLabelText('comment-input')).toHaveValue('');
+    expect(mockDefaultProps.onSubmit).toHaveBeenCalledTimes(1);
+  });
+
+  it('should show error message when submission failed', async () => {
+    mockDefaultProps.onSubmit.mockResolvedValueOnce({ error: 'failed' });
+
+    const { getByLabelText, getByText } = render(<CommentForm {...mockDefaultProps} />);
+
+    fireEvent.change(getByLabelText('comment-input'), {
+      target: { value: 'new comment' },
+    });
+
+    await act(async () => {
+      fireEvent.click(getByLabelText('submit-comment-button'));
+    });
+
+    expect(getByLabelText('comment-input')).toHaveValue('new comment');
+    expect(getByText('Oops! Something went wrong...')).toBeInTheDocument();
+    expect(mockDefaultProps.onSubmit).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ fujiji-client/src/components/CommentForm/CommentForm.stories.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import CommentForm from './CommentForm';
+
+export default {
+  title: 'CommentForm',
+  component: CommentForm,
+};
+
+function Template({ ...args }) {
+  return <CommentForm {...args} />;
+}
+
+export const Default = Template.bind({});
+Default.args = {
+  userName: 'John Doe',
+  onSubmit: () => ({ status: 200 }),
+};
+
+export const ApiError = Template.bind({});
+ApiError.args = {
+  userName: 'John Doe',
+  onSubmit: () => ({ error: {} }),
+};
