9f6affc8fb0ba06adfd2653e454837c914d2d605
Jishan Arora <43257870+jishanarora@users.noreply.github.com>
2022-04-03 16:40:25 -0500

    Update index.jsx

--- fujiji-client/src/pages/index.jsx
+++ fujiji-client/src/pages/index.jsx
@@ -10,9 +10,7 @@ import {
 } from '@chakra-ui/react';
 
 export default function Home() {
-  console.log(process.env.FUJIJI_API_URL);
-  console.log(process.env.REACT_APP_FUJIJI_API_URL);
-  console.log(process.env.APPSETTING_FUJIJI_API_URL);
+  console.log(process.env.NEXT_PUBLIC_FUJIJI_API_URL);
 
   return (
     <div>
