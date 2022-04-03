a31f2d7036a51d41720e039e82280e5e3617b5cc
Jishan Arora <43257870+jishanarora@users.noreply.github.com>
2022-04-03 16:38:00 -0500

    Update next.config.js

--- fujiji-client/next.config.js
+++ fujiji-client/next.config.js
@@ -3,8 +3,8 @@ module.exports = {
   env: {
     // NOTE: this is exposing environment variables on the client side
     // don't expose secrets to client
-    FUJIJI_API_URL: process.env.FUJIJI_API_URL,
-    JWT_AUTH_TOKEN: process.env.JWT_AUTH_TOKEN,
+    FUJIJI_API_URL: process.env.NEXT_PUBLIC_FUJIJI_API_URL,
+    JWT_AUTH_TOKEN: process.env.NEXT_PUBLIC_JWT_AUTH_TOKEN,
   },
    eslint: {
     // Warning: This allows production builds to successfully complete even if
