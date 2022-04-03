4668799b51aedc33d8196322405bea1e42cbea85
Jishan Arora <43257870+jishanarora@users.noreply.github.com>
2022-04-03 16:39:12 -0500

    Update index.js

--- fujiji-client/config/index.js
+++ fujiji-client/config/index.js
@@ -1,6 +1,6 @@
 const config = {
-  FUJIJI_API_URL: process.env.FUJIJI_API_URL,
-  JWT_AUTH_TOKEN: process.env.JWT_AUTH_TOKEN,
+  FUJIJI_API_URL: process.env.NEXT_PUBLIC_FUJIJI_API_URL,
+  JWT_AUTH_TOKEN: process.env.NEXT_PUBLIC_JWT_AUTH_TOKEN,
 };
 
 export default config;
