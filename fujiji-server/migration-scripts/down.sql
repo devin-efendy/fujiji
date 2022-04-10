349757a1dd13cc633409b2708a72bccb7ce35708
wynonagcc <wynonagoldiella@gmail.com>
2022-04-09 20:47:42 -0500

    Feat/#142 Create boost & boost_package tables

--- fujiji-server/migration-scripts/down.sql
+++ fujiji-server/migration-scripts/down.sql
@@ -1,8 +1,11 @@
 USE fujiji
 
+DROP TABLE IF EXISTS fujiji_comments;
+DROP TABLE IF EXISTS fujiji_boost;
 DROP TABLE IF EXISTS fujiji_listing;
 DROP TABLE IF EXISTS fujiji_user;
-DROP TABLE IF EXISTS fujiji_comments;
+DROP TABLE IF EXISTS fujiji_boost_package;
+
 GO
 
 DROP DATABASE fujiji;
