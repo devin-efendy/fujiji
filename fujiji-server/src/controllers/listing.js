72758a8cd05da26fba2e225631cd1ce3cb4bf356
Asifur Rahman <asif_rahman@Asifurs-MacBook-Air.local>
2022-04-20 11:15:51 -0500

    Sort listings based on score and creation date

--- fujiji-server/src/controllers/listing.js
+++ fujiji-server/src/controllers/listing.js
@@ -230,6 +230,7 @@ async function getAllListings(req, res, next) {
       next(listings);
       return;
     }
+    /*
     for (let index = 0; index < listings.length; index += 1) {
       // eslint-disable-next-line no-await-in-loop
       const boost = await getBoostByListingId(listings[index].listing_id);
@@ -239,6 +240,7 @@ async function getAllListings(req, res, next) {
         listings[index].score = boost.score;
       }
     }
+    */
     res.status(200).json({ listings });
   } catch (err) {
     next(new APIError());
